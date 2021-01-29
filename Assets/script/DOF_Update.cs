ed2a3bccac47b847a17ede1eca859cd3ff3b366c
jackng221 <jackng1233@gmail.com>
2021-01-29 19:32:17 +0800

    Dynamic_DOF

---
+++ Assets/script/DOF_Update.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class DOF_Update : MonoBehaviour
+{
+    float currentHitDistance;
+    private PostProcessVolume postProcessVolume;
+    DepthOfField dof;
+
+    private void Start()
+    {
+        postProcessVolume = GetComponent<PostProcessVolume>();
+
+    }
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            currentHitDistance = hit.distance;
+        }
+        else
+        {
+            currentHitDistance = 10;
+        }
+        if (postProcessVolume)
+        {
+            if (postProcessVolume.sharedProfile.TryGetSettings<DepthOfField>(out dof)){
+                dof.focusDistance.value = Mathf.Lerp(dof.focusDistance.value, currentHitDistance, Time.deltaTime * 1.5f);
+            }
+        }
+    }
+}
