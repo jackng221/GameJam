45a9b4322923d500c716086e7fc659b30d7e0f6f
jackng221 <jackng1233@gmail.com>
2021-01-30 16:31:50 +0800

    Lv1Scene+FPS
    Lv1 scene
    FPS Jack ver
    flashlight aiming (to be tested)

---
+++ Assets/script/FPSController/FlashLightLookAt.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class FlashLightLookAt : MonoBehaviour
+{
+    public GameObject flashLightObj;
+    public Camera cam;
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit)){
+            flashLightObj.transform.DOLookAt(hit.point, 0.5f);
+        }
+        //else
+        //{
+        //    flashLightObj.transform.DOLookAt(cam.transform.forward, 0.5f);
+        //}
+    }
+}
