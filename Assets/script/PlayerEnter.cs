8777ee758ecd0f5ca76567aea0e4c5a0921b434f
crapper <crapper.612@gmail.com>
2021-01-30 18:09:25 +0800

    30/1/2021

--- Assets/script/PlayerEnter.cs
+++ Assets/script/PlayerEnter.cs
@@ -13,7 +13,7 @@ public class PlayerEnter : MonoBehaviour
     public GameObject Dialog, conversate, lv1manager, objectmanager;
     GameObject player;
     [SerializeField]
-    int isdoor = 0;
+    int isdoor = 0, iswall= 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,58 +23,69 @@ public class PlayerEnter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (isenter == true && Input.GetKeyDown(KeyCode.C) && Dialog.gameObject.activeInHierarchy != true && player.GetComponent<FPSMovement>().ischanging == false && isdoor == 0)
-        {
-            GameObject.FindGameObjectWithTag("Player").GetComponent<FPSMovement>().ismenuopen = true;
-            for (int i = 0; i < input.Count; i++)
+        if (iswall == 1 && this.gameObject.transform.GetChild(0).gameObject.activeInHierarchy == true) { 
+            if (isenter == true && Input.GetKeyDown(KeyCode.C) && Dialog.gameObject.activeInHierarchy != true && player.GetComponent<FPSMovement>().ischanging == false && isdoor == 0)
             {
-                Dialog.GetComponent<TextControl>().text.Add(input[i]);
-                Dialog.GetComponent<TextControl>().type = 0;
-            }
-            Dialog.gameObject.SetActive(true);
-            conversate.SetActive(false);
-        }
-        else if (isenter == true && Input.GetKeyDown(KeyCode.C) && Dialog.gameObject.activeInHierarchy != true && player.GetComponent<FPSMovement>().ischanging == false && isdoor > 0) {
-            GameObject.FindGameObjectWithTag("Player").GetComponent<FPSMovement>().ismenuopen = true;
-            if (isdoor == 1 && lv1manager.GetComponent<Level1Manager>().Level1iswin) {
-                for (int i = 0; i < input2.Count; i++)
-                {
-                    Dialog.GetComponent<TextControl>().text.Add(input2[i]);
-                    Dialog.GetComponent<TextControl>().type = 2;
-                }
-            }else if (isdoor == 2 && objectmanager.GetComponent<Level2Manager>().winLevel2)
-            {
-                for (int i = 0; i < input2.Count; i++)
-                {
-                    Dialog.GetComponent<TextControl>().text.Add(input2[i]);
-                    Dialog.GetComponent<TextControl>().type = 2;
-                }
-            }else if (isdoor == 3)
-            {
-                for (int i = 0; i < input2.Count; i++)
-                {
-                    Dialog.GetComponent<TextControl>().text.Add(input2[i]);
-                    Dialog.GetComponent<TextControl>().type = 2;
-                }
-            } else {
+                GameObject.FindGameObjectWithTag("Player").GetComponent<FPSMovement>().ismenuopen = true;
                 for (int i = 0; i < input.Count; i++)
                 {
                     Dialog.GetComponent<TextControl>().text.Add(input[i]);
                     Dialog.GetComponent<TextControl>().type = 0;
                 }
+                Dialog.gameObject.SetActive(true);
+                conversate.SetActive(false);
             }
-            Dialog.gameObject.SetActive(true);
-            conversate.SetActive(false);
+            else if (isenter == true && Input.GetKeyDown(KeyCode.C) && Dialog.gameObject.activeInHierarchy != true && player.GetComponent<FPSMovement>().ischanging == false && isdoor > 0)
+            {
+                GameObject.FindGameObjectWithTag("Player").GetComponent<FPSMovement>().ismenuopen = true;
+                if (isdoor == 1 && lv1manager.GetComponent<Level1Manager>().Level1iswin)
+                {
+                    for (int i = 0; i < input2.Count; i++)
+                    {
+                        Dialog.GetComponent<TextControl>().text.Add(input2[i]);
+                        Dialog.GetComponent<TextControl>().type = 2;
+                    }
+                }      
+                else if (isdoor == 2 && objectmanager.GetComponent<Level2Manager>().winLevel2)
+                {
+                    for (int i = 0; i < input2.Count; i++)
+                    {
+                        Dialog.GetComponent<TextControl>().text.Add(input2[i]);
+                        Dialog.GetComponent<TextControl>().type = 2;
+                    }
+                }
+                else if (isdoor == 3)
+                {
+                    for (int i = 0; i < input2.Count; i++)
+                    {
+                        Dialog.GetComponent<TextControl>().text.Add(input2[i]);
+                        Dialog.GetComponent<TextControl>().type = 2;
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < input.Count; i++)
+                    {
+                        Dialog.GetComponent<TextControl>().text.Add(input[i]);
+                        Dialog.GetComponent<TextControl>().type = 0;
+                    }
+                }
+                Dialog.gameObject.SetActive(true);
+                conversate.SetActive(false);
 
-        }
+            }
+         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.tag == "Player")
         {
-            if (Dialog.gameObject.activeInHierarchy == false)
+            if (Dialog.gameObject.activeInHierarchy == false && iswall == 0)
             {
+                conversate.SetActive(true);
+            }
+            else if (Dialog.gameObject.activeInHierarchy == false && iswall == 1 && this.gameObject.transform.GetChild(0).gameObject.activeInHierarchy == true) {
                 conversate.SetActive(true);
             }
             isenter = true;
